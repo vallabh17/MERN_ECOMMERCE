3291481b6a52da01fff0e4bbe4f8aa9078224b62
Vallabh Gudsoorkar <97428581+vallabh17@users.noreply.github.com>
2024-08-01 03:37:51 +0530

    Update db.js

--- backend-zip/src/config/db.js
+++ backend-zip/src/config/db.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose")
 
 const mongoDbUrl='mongodb+srv://vallabhgudsoorkar17:VnRNQgP2aVtlxROL@cluster0.pwdajcu.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0'
-//'mongodb+srv://codewithzosh:lognBCBmtWNPjrKC@cluster0.wvt9jpw.mongodb.net/?retryWrites=true&w=majority'
 const connectDb=()=>{
     return mongoose.connect(mongoDbUrl)
 }
